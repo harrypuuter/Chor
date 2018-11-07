81d93d2b60be4dd535d7fa7377c9ebdb81f96b5d
Brommer <brommer.sebastian@gmail.com>
2018-11-07 18:03:20 +0100

    cleanup

--- update-afi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git add .
-git commit -a -m "added new song(s)"
-git pull christian gh-pages
-git commit -a -m "added christians song(s)"
-python generate.py
-git commit -a -m "updated index"
-git push christian master:gh-pages
--- update-christian.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git add .
-git commit -a -m "added new song(s)"
-git pull --no-edit afi master
-git pull --no-edit origin gh-pages
-python generate.py
-git commit -a -m "updated index"
-git push origin gh-pages
